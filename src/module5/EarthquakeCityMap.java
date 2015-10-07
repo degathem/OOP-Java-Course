322d87e24406fefc61f53e0909d0fae7a74c1ed2
Cam Gillespie <gillespie.cam@gmail.com>
2015-10-07 13:28:41 -0600

    Hover Select method functioning

--- src/module5/EarthquakeCityMap.java
+++ src/module5/EarthquakeCityMap.java
@@ -146,6 +146,16 @@ public class EarthquakeCityMap extends PApplet {
 	private void selectMarkerIfHover(List<Marker> markers)
 	{
 		// TODO: Implement this method
+		for (Marker marker: markers) {
+			if (marker.isInside(map, mouseX, mouseY)) {
+				lastSelected = (CommonMarker)marker;
+				lastSelected.setSelected(true);
+				//System.out.println(lastSelected.getProperty("name"));
+			}
+			
+		}
+		
+		
 	}
 	
 	/** The event handler for mouse clicks
